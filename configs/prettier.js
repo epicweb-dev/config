40fc0b2816d73260f9586ec964ff98809be44953
Kent C. Dodds <me+github@kentcdodds.com>
2024-05-25 09:44:19 -0600

    fix(prettier): format mdx properly

--- configs/prettier.js
+++ configs/prettier.js
@@ -27,6 +27,17 @@ export default {
 				useTabs: false,
 			},
 		},
+		{
+			files: ['**/*.mdx'],
+			options: {
+				// This stinks, if you don't do this, then an inline component on the
+				// end of the line will end up wrapping, then the next save prettier
+				// will add an extra line break. Super annoying and probably a bug in
+				// prettier, but until it's fixed, this is the best we can do.
+				proseWrap: 'preserve',
+				htmlWhitespaceSensitivity: 'ignore',
+			},
+		},
 	],
 	plugins: ['prettier-plugin-tailwindcss'],
 	tailwindAttributes: ['class', 'className', 'ngClass', '.*[cC]lassName'],
